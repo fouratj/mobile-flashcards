0c183120fbede4f24816723613f794d18cbc228e
Fourat Janabi <fouratj@gmail.com>
2017-10-15 20:22:44 -0200

    refactor: Remove unused reducer

--- store/actions.js
+++ store/actions.js
@@ -2,13 +2,6 @@ export const ADD_DECKS = "ADDD_DECKS"
 export const ADD_DECK = "ADD_DECK"
 export const ADD_CARD = "ADD_CARD"
 
-export function addDecks(decks) {
-  return {
-    type: ADD_DECKS,
-    decks
-  }
-}
-
 export function addDeck(deck) {
   return {
     type: ADD_DECK,

7e8525064ef12cc7d88ba938799f0a380e4d6048
Fourat Janabi <fouratj@gmail.com>
2017-10-15 20:22:30 -0200

    feat: Add Quiz component to display questions

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,16 +1,48 @@
 import React from 'react';
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import { connect } from 'react-redux'
+
+function Question ({ question }) {  
+  return (
+    <View>
+        <Text style={styles.header}>{question}</Text>
+    </View>
+  )
+}
 
 class Quiz extends React.Component {
+
   render () {
+    const { deckId } = this.props.navigation.state.params
+    const { decks } = this.props
+    const deck = decks[deckId]
+    
     return (
-    <View>
-      <Text>{
-        this.props.navigation.state.params.deckId
-      }</Text>
-    </View>
+      <View style={{flex: 1}}>
+        { deck.questions.length && deck.questions.map(q => {
+          return <Question question={q.question} key={q.question} />
+          })
+        }
+      </View>
     )
   }
 }
 
-export default Quiz
+const styles = StyleSheet.create({
+  header: {
+    fontSize: 36,
+    textAlign: 'center',
+    shadowRadius: 3,
+  },
+  text: {
+    textAlign: 'center'
+  }
+})
+
+const mapStateToProps = (state) => {
+  return {
+    decks: state
+  }
+}
+
+export default connect(mapStateToProps, null)(Quiz)

ab6c7d77821f1579117d1412f0c85b5ad94ca65e
Fourat Janabi <fouratj@gmail.com>
2017-10-21 16:58:30 -0200

    feat: Add notification functionality

---
+++ utils/notifications.js
@@ -0,0 +1,48 @@
+import { AsyncStorage } from 'react-native'
+import { Notifications, Permissions } from 'expo'
+
+
+const key = 'mobile-flashcards:notifications'
+
+export function clearLocalNotification() {
+  return AsyncStorage.removeItem(key)
+    .then(Notifications.cancelAllScheduledNotificationsAsync)
+}
+
+function createNotification() {
+  return {
+    title: 'Study',
+    body: "Study tip: Make sure you study",
+    ios: {
+      sound: true,
+    }
+  }
+}
+
+export function setLocalNotification() {
+  AsyncStorage.getItem(key)
+    .then(JSON.parse)
+    .then((data) => {
+      if (data === null) {
+        Permissions.askAsync(Permissions.NOTIFICATIONS)
+          .then(({ status }) => {
+            if (status === 'granted') {
+              Notifications.cancelAllScheduledNotificationsAsync()
+
+              let tomorrow = new Date()
+              tomorrow.setDate(tomorrow.getDate() + 1)
+              tomorrow.setHours(18)
+              tomorrow.setMintutes(30)
+
+              Notifications.scheduleLocalNotificationAsync(
+                createNotification(),
+                { time: tomorrow,
+                  repeat: 'week',
+                })
+
+              AsyncStorage.setItem(key, JSON.stringify(true))
+            }
+          })
+      }
+    })
+}

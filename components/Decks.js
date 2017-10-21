bd30c2b91905ec7f156e735cf85d4eb0616bfc39
Fourat Janabi <fouratj@gmail.com>
2017-10-21 16:17:36 -0200

    feat: Implement scrollView

--- components/Decks.js
+++ components/Decks.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity, StyleSheet } from 'react-native'
 import { connect } from 'react-redux'
 import Deck from './Deck'
 import DeckInfo from './DeckInfo'
@@ -27,31 +27,31 @@ class Decks extends Component {
     if (decks)
       decks = Object.entries(decks).map(d => d[1])
 
+    decks = decks.map((deck, i) => {
+      return (
+        <View style={( i % 2 === 0) ? styles.light : styles.grey} key={i}>
+          <DeckInfo 
+            deck={deck}
+            onPress={() => this.props.navigation.navigate(
+              'Deck',
+              { deck: deck.title }
+            )} 
+          />
+        </View>   
+      )
+    })
+
     if (decks.length) {
-      return(
-        <View style={styles.container}>
-          {
-            decks.map((deck, i) => {
-              return (
-                <DeckInfo 
-                  style={styles.deck}
-                  key={i}
-                  deck={deck} 
-                  onPress={() => this.props.navigation.navigate(
-                    'Deck',
-                    { deck: deck.title }
-                  )} 
-                  />
-              )
-            })
-          }
-        </View>
+      return (
+        <ScrollView>
+          {decks}
+        </ScrollView>
       )
     }
 
     return (
       <View>
-        <Text>No decks here...</Text>
+        <Text>Nothing to see here...move along.</Text>
       </View>
     )
   }
@@ -63,16 +63,21 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
   },
   deck: {
-    flex: 1,
-    paddingTop: 20
+    padding: 10,
+    backgroundColor: 'red',
+    margin: 20,
+  },
+  light: {
+    backgroundColor: '#f7f8f9',
+    padding: 20
+  },
+  grey: {
+    backgroundColor: '#cbd1db',
+    padding: 20
   },
   text: {
     textAlign: 'center'
   },
-  deck: {
-    marginTop: 30,
-    marginBottom: 30
-  }
 })
 
 const mapStateToProps = (state) => {
